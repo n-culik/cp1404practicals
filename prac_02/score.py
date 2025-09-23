bf3d29095646077fe5057640f63fe45b384188aa
n-culik <nicola.culik@my.jcu.edu.au>
2025-09-23 16:14:16 +0800

    Refractor score validation program to use functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,13 +2,27 @@
 CP1404/CP5632 - Practical
 Program to determine score status
 """
+import random
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    score = float(input("Enter score: "))
+    result = validate_score(score)
+    print(f"your score is {result}")
+
+    random_score = random.randint(0, 100)
+    result = validate_score(random_score)
+    print(f"Your score is {result}")
+
+
+def validate_score(score: float):
+    if score < 0 or score > 100:
+        result = "Invalid score"
+    elif score >= 90:
+        result = "Excellent"
+    elif score >= 50:
+        result = "Passable"
+    else:
+        result = "Bad"
+    return result
+
+main()
