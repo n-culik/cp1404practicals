e3754eef7f6426606b1f468bc57619d0c74ddd06
n-culik <nicola.culik@my.jcu.edu.au>
2025-09-23 16:16:01 +0800

    Remove getpass import in password validation file

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -4,8 +4,6 @@ Date: 22.09.2025
 Author: Nicola Culik
 Description: In this Program we will ask the user for a password and print it with asteriks
 """
-import getpass
-
 MIN_PASSWORD_LENGTH = 8
 SPECIAL_CHARACTER = "*"
 
