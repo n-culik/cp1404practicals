e96d49d2b9348991530a339bb6f65c7f5977776f
n-culik <nicola.culik@my.jcu.edu.au>
2025-09-30 17:10:04 +0800

    Change constants in capitalist conrad

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -11,10 +11,10 @@ Date : 30.09.2025
 
 import random
 
-MAX_INCREASE = 0.1  # 10%
+MAX_INCREASE = 0.175  # 17.5%
 MAX_DECREASE = 0.05  # 5%
-MIN_PRICE = 0.01
-MAX_PRICE = 1000.0
+MIN_PRICE = 1.0
+MAX_PRICE = 100.0
 INITIAL_PRICE = 10.0
 
 number_of_days = 0
