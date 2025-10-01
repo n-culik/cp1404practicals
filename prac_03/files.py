5642a4aefc3ced84d4ef6ce42cfd1cab0f1481e7
n-culik <nicola.culik@my.jcu.edu.au>
2025-10-01 10:46:01 +0800

    Add code for 3 and 4 exercises in files prac

--- prac_03/files.py
+++ prac_03/files.py
@@ -9,22 +9,39 @@ Description: Get some experience in file reading and writing
 1.
 Using write to ask the user for a name and create a new file with the name in it
 """
-user_name = input("Enter your name: ")
-file = open("name.txt", "w")
-file.write(user_name)
-file.close()
-print()
+# user_name = input("Enter your name: ")
+# file = open("name.txt", "w")
+# file.write(user_name)
+# file.close()
+# print()
 
 """
 2.
 Using read to get the name
 """
-file = open("name.txt", "r")
-name = file.read().strip()
-print(f"Hi {name}!")
-file.close()
+# file = open("name.txt", "r")
+# name = file.read().strip()
+# print(f"Hi {name}!")
+# file.close()
 
 """
 3.
 Using with to open a file and get the first two numbers
 """
+result = 0
+with open("numbers.txt", "r") as file:
+    for i in range(1,3):
+        number = int(file.readline())
+        result += number
+    print(result)
+
+"""
+4.
+Using with to open a file and add all numbers in the file to the result
+"""
+result = 0
+with open("numbers.txt", "r") as file:
+    for line in file:
+        number = int(line)
+        result += number
+    print(result)
