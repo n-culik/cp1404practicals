46565b7b52121014f3b2ccdfe303eff9a7c7ff38
n-culik <nicola.culik@my.jcu.edu.au>
2025-10-01 10:37:07 +0800

    Add code for first 2 exercises in files prac

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,6 +1,30 @@
 """
 Module : CP1404
-Date: XX.09.2025
+Date: 01.10.2025
 Author: Nicola Culik
-Description:
+Description: Get some experience in file reading and writing
+"""
+
+"""
+1.
+Using write to ask the user for a name and create a new file with the name in it
+"""
+user_name = input("Enter your name: ")
+file = open("name.txt", "w")
+file.write(user_name)
+file.close()
+print()
+
+"""
+2.
+Using read to get the name
+"""
+file = open("name.txt", "r")
+name = file.read().strip()
+print(f"Hi {name}!")
+file.close()
+
+"""
+3.
+Using with to open a file and get the first two numbers
 """
