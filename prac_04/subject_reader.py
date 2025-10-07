0b585b007bb6af954ecd91bfc74d7565bc8a92e2
n-culik <nicola.culik@my.jcu.edu.au>
2025-10-07 15:50:57 +0800

    Create function print subject info for subject reader

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,25 +7,32 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data(FILENAME)
-    print(data)
+    subject_infos = load_data(FILENAME)
+    print_subject_info(subject_infos)
 
 
 def load_data(filename=FILENAME):
     """Read data from file formatted like: subject,lecturer,number of students."""
-    parts_pair = []
+    subject_infos = []
     input_file = open(filename)
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
+        subject_info = line.split(',')  # Separate the data into its subject_info
+        print(subject_info)  # See what the subject_info look like (notice the integer is a string)
+        subject_info[2] = int(subject_info[2])  # Make the number an integer (ignore PyCharm's warning)
+        print(subject_info)  # See if that worked
         print("----------")
-        parts_pair.append(parts)
+        subject_infos.append(subject_info)
     input_file.close()
-    return parts_pair
+    return subject_infos
 
+def print_subject_info(subject_infos):
+    """Print structured subject info."""
+    teacher_lenght = max(len(subject_info[1]) for subject_info in subject_infos)
+    number_of_student_lenght = max(len(str(subject_info[2])) for subject_info in subject_infos)
+
+    for subject in subject_infos:
+        print(f"{subject[0]} is taught by {subject[1]:{teacher_lenght}} and has {subject[2]:{number_of_student_lenght}} students")
 main()
