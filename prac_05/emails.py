5d5073fc8c222ed58e96a27e8507c9afa07e85fc
n-culik <nicola.culik@my.jcu.edu.au>
2025-10-15 15:04:01 +0800

    Extract the name from the email address

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -7,20 +7,20 @@ Estimate: 50 min
 Actual:  min
 """
 
-
 def main():
     email_to_name = {}
-    email = input("Enter e-mail address: ")
+    print(email_to_name)
+    email = input("Email: ")
     email_to_name[email] = extract_name(email)
     while email != '':
-        email = input("Enter e-mail address: ")
+        email = input("Email: ")
         email_to_name[email] = extract_name(email)
     print(email_to_name)
 
 def extract_name(email):
     remove_mail = email.split("@")
     names = remove_mail[0].split(".")
-    whole_name = (remove_mail[0] + " " + remove_mail[1])
+    whole_name = " ".join(names).title()
     return whole_name
 
 main()
