78babf49aa3f223d35374a5f212949a1b2a66195
n-culik <nicola.culik@my.jcu.edu.au>
2025-10-15 11:42:55 +0800

    Change from LBYL to EAFP in state names

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -8,13 +8,19 @@ Date: 15.10.2025
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
+is_valid = False
+
 for code, name in CODE_TO_NAME.items():
     print(f"{code:3} is {name}")
 
 state_code = input("Enter short state: ").upper()
+
 while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
-        print("Invalid short state")
+    while not is_valid:
+        try:
+            print(state_code, "is", CODE_TO_NAME[state_code])
+            is_valid = True
+        except KeyError:
+            print("Invalid short state")
+            state_code = input("Enter short state: ").upper()
     state_code = input("Enter short state: ").upper()
