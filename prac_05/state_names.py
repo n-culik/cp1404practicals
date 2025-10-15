013b481f1a0edd48056803ab1b40488bd1768e17
n-culik <nicola.culik@my.jcu.edu.au>
2025-10-15 11:55:24 +0800

    Add loop for empty input validation

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -8,7 +8,7 @@ Date: 15.10.2025
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
-is_valid = False
+
 
 for code, name in CODE_TO_NAME.items():
     print(f"{code:3} is {name}")
@@ -16,11 +16,13 @@ for code, name in CODE_TO_NAME.items():
 state_code = input("Enter short state: ").upper()
 
 while state_code != "":
+    is_valid = False
     while not is_valid:
         try:
             print(state_code, "is", CODE_TO_NAME[state_code])
             is_valid = True
         except KeyError:
             print("Invalid short state")
-            state_code = input("Enter short state: ").upper()
-    state_code = input("Enter short state: ").upper()
+        state_code = input("Enter short state: ").upper()
+        if state_code == "":
+            is_valid = True
