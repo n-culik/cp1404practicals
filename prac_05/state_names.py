92c1c692a7c5f84419e1bd029596cec0fecf09c2
n-culik <nicola.culik@my.jcu.edu.au>
2025-10-16 13:19:20 +0800

    Remove todo string in state names

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,8 +4,6 @@ State names in a dictionary
 File needs reformatting
 Date: 15.10.2025
 """
-
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
 
