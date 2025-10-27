08a574f6dd4ee47cdb6e9c7c777a767e13a4a519
n-culik <nicola.culik@my.jcu.edu.au>
2025-10-27 15:14:30 +0800

    Create str method in language class

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -14,6 +14,9 @@ class ProgrammingLanguage():
         self.reflection = reflection
         self.year = year
 
+    def __str__(self):
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
     def is_dynamic(self):
         if self.typing == 'Dynamic':
             return True
