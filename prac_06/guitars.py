4511278945da8b17fb8e9e3a40cbea183c3480f3
n-culik <nicola.culik@my.jcu.edu.au>
2025-10-27 18:01:47 +0800

    Add user input code to create guitars in a loop until empty name is entered

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -4,20 +4,26 @@ Email name extraction
 Date: 27.10.2025
 Author: Nicola Culik
 Estimate: 50 min
-Actual:   min
+Actual:  20 min
 """
 
 from guitar import Guitar
 
 def main():
     guitars = []
-    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        name = input("Name: ")
+
 
     for i, guitar in enumerate(guitars, 1):
         vintage_string = " (vintage)" if guitar.is_vintage() else ""
         print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
-
-
 
 main()
