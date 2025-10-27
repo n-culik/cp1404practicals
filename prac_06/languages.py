e50aa488e4f0163e26297c5c88788d4a28dfbfd5
n-culik <nicola.culik@my.jcu.edu.au>
2025-10-27 15:13:08 +0800

    import basic example code for programming languages

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -6,3 +6,9 @@ Author: Nicola Culik
 Estimate: 50 min
 Actual:   min
 """
+from programming_language import ProgrammingLanguage
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
