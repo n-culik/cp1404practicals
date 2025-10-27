51b08e70cfd2d026f93df0eb297c2224ee5352b0
n-culik <nicola.culik@my.jcu.edu.au>
2025-10-27 15:02:59 +0800

    Add name as attribute to the class

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -9,17 +9,17 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car("Mercedes", 180)
     my_car.drive(30)
-    print(f"Car has fuel: {my_car.fuel}")
+    print(f"{my_car.name} has fuel: {my_car.fuel}")
     print(my_car)
 
     # Q1
-    limo = Car(100)
+    limo = Car("Lexus", 100)
     # Q2
     limo.add_fuel(20)
     # Q3
-    print(f"Limo fuel: {limo.fuel}")
+    print(f"{my_car.name} has fuel: {limo.fuel}")
     # Q4
     limo.drive(115)
     # Q5
