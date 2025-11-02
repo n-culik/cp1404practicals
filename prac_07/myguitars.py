8c79e731e1231467b54f6265045176b6ff7389a8
n-culik <nicola.culik@my.jcu.edu.au>
2025-11-02 16:24:20 +0800

    Add code to write to file

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -4,7 +4,7 @@ Guitar code
 Date: 02.11.2025
 Author: Nicola Culik
 Estimate: 50 min
-Actual:   min
+Actual:  24 min
 """
 from guitar import Guitar
 FILENAME = "guitars.csv"
@@ -14,8 +14,9 @@ def main():
     print("Your current guitars are:")
     guitars.sort()
     print_guitar(guitars)
+    print()
 
-    print("Add a new guitar:")
+    print("Add a new guitar")
     name = input("Name of the guitar:")
     while name !="":
         year = int(input("Year of guitar:"))
@@ -23,15 +24,16 @@ def main():
         guitars.append(Guitar(name, year, price))
         name = input("Name of new guitar:")
 
+    print()
     print("Your new guitars are:")
     guitars.sort()
     print_guitar(guitars)
 
+    write_file(guitars)
 
 def print_guitar(guitars):
     for guitar in guitars:
         print(guitar)
-
 
 def open_file():
     guitars = []
@@ -44,5 +46,10 @@ def open_file():
             guitars.append(guitar)
     return guitars
 
+def write_file(guitars):
+    with open(FILENAME, "w") as out_file:
+        for guitar in guitars:
+            csv_data = f"{guitar.name},{guitar.year},{guitar.cost}\n"
+            out_file.write(f"{csv_data}")
 
 main()
