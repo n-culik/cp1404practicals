5bf744a1e688525068791fdc97dd9d167a9e0d0b
n-culik <nicola.culik@my.jcu.edu.au>
2025-11-02 16:00:59 +0800

    Base structure open file and get parts in guitar

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -6,3 +6,23 @@ Author: Nicola Culik
 Estimate: 50 min
 Actual:   min
 """
+from guitar import Guitar
+FILENAME = "guitars.csv"
+
+def main():
+    guitars = []
+
+    data = open_file()
+    for line in data:
+        parts = line.strip().split(',')
+        guitar = Guitar(parts[0], parts[1], parts[2])
+        guitars.append(guitar)
+
+
+def open_file():
+    with open(FILENAME, "r") as in_file:
+        data = in_file.readlines()
+    return data
+
+
+main()
