0a68f7bb0acfa4706fb78cce633a55d67c7c3c68
n-culik <nicola.culik@my.jcu.edu.au>
2025-11-04 09:33:54 +0800

    Create files for project management

---
+++ prac_07/project.py
@@ -0,0 +1,8 @@
+"""
+CP1404 Practical
+Project class
+Date: 04.11.2025
+Author: Nicola Culik
+Estimate: 40 min
+Actual:   min
+"""
---
+++ prac_07/project_management.py
@@ -0,0 +1,16 @@
+"""
+CP1404 Practical
+Project management code to handle projects
+Date: 04.11.2025
+Author: Nicola Culik
+Estimate: 300 min
+Actual:   min
+"""
+
+MENU="""- (L)oad projects  
+- (S)ave projects  
+- (D)isplay projects  
+- (F)ilter projects by date
+- (A)dd new project  
+- (U)pdate project
+- (Q)uit"""
