13b7a16ec49cbffea3fe70d8c96a6c13f6113a2b
n-culik <nicola.culik@my.jcu.edu.au>
2025-11-10 14:05:31 +0800

    Add function to build labels. import kivy file content from prac

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -9,12 +9,24 @@ Actual:  min
 
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.label import Label
+from kivy.core.window import Window
 
 class DynamicLabelsApp(App):
     def build(self):
-        """ build the Kivy app from the kv file """
+        """build the Kivy app from the kv file"""
+        Window.size = (500, 300)
         self.title = "Dynamic Labels"
         self.root = Builder.load_file('dynamic_labels.kv')
+        self.build_labels()
         return self.root
+
+    def build_labels(self):
+        """"""
+        names = ["Nicola", "Bob", "Terry", "Test"]
+        for name in names:
+            temp_label = Label(text=name)
+            temp_label.background_color = (0.22, 0.44, 0.11, 1)
+            self.root.ids.main.add_widget(temp_label)
 
 DynamicLabelsApp().run()
