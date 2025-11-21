f6c197a9392bee7ff6666232ddb7b552d70f5ec8
n-culik <nicola.culik@my.jcu.edu.au>
2025-11-21 18:37:30 +0800

    Add taxi tests

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,20 @@
+"""
+CP1404 Practical
+Taxi class test code
+Author: Nicola Culik
+"""
+
+from taxi import Taxi
+
+def main():
+    my_taxi = Taxi("Prius 1", 100, 1.23)
+    print(my_taxi)
+    my_taxi.drive(40)
+    print(my_taxi)
+    print(f"Current fare: ${my_taxi.get_fare():.2f}")
+    print("New fare")
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+    print(my_taxi)
+    print(f"Current fare: ${my_taxi.get_fare():.2f}")
+main()
