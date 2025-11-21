a42a15f1e86830ffb65a1da5f53f12f8dd5afd61
n-culik <nicola.culik@my.jcu.edu.au>
2025-11-21 19:00:48 +0800

    Change drive function to use if else

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -7,17 +7,18 @@ Author: Nicola Culik
 import random
 from car import Car
 
-
-
 class UnreliableCar(Car):
     def __init__(self, name, fuel, reliability):
         super().__init__(name, fuel)
         self.reliability = reliability
 
     def drive(self, distance):
-        drive_probability = random.randint(0, 1)
-        if drive_probability < self.reliability:
+        drive_probability = random.randint(0, 100)
+        if  drive_probability < self.reliability:
             distance_driven = super().drive(distance)
-        return distance_driven
+            return distance_driven
+        else:
+            return 0
+
 
 
