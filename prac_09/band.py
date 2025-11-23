1b89a9b05f80fb9ab69eadabdd3f6b9f74d5a2ca
n-culik <nicola.culik@my.jcu.edu.au>
2025-11-23 12:07:44 +0800

    Create Band class file and add basic construct and display methods

---
+++ prac_09/band.py
@@ -0,0 +1,18 @@
+"""
+Band class for CP1404
+Author: Nicola Culik
+"""
+
+
+class Band:
+    """Band class"""
+
+    def __init__(self, name=""):
+        """Construct a Band with a name and empty musician collection."""
+        self.name = name
+        self.musicians = []
+
+    def __str__(self):
+        """Return a string representation of a Band."""
+        return f"{self.name} ({self.musicians})"
+
